31f57fab1d2b2800175007373abc57803ff53ced
DSHardman <dshardman@outlook.com>
2024-05-01 10:19:42 +0100

    running pre-meeting repeats

--- PrinterControl.py
+++ PrinterControl.py
@@ -9,7 +9,7 @@ targetforce = 0.3  # In N
 retractheight = 15
 step = 0.05
 waittime = 5
-savestring = "RawData/GridFillProbing"
+savestring = "RawData/Radial2"
 
 Ender = serial.Serial("COM10", 115200)
 Forces = serial.Serial("COM9", 115200)
@@ -65,98 +65,113 @@ def setup():
     waitforposition()
 
 def main():
-    # # Random probing at 0.3N
-    # targetforce = 0.3  # In N
-    # for i in range(1000):
-    #     print(i)
-    #     x = 18.16*random.random() + 1  # 22.86 side length
-    #     y = 25.78*random.random() + 1  # 30.48 side length
-    #     Ender.write(str.encode("G1 X "+str(Cornerposition[0]+x)+" Y "+str(Cornerposition[1]+y)+" F800\r\n"))
-    #     waitforposition()
-    #     times = takereading(targetforce)
-    #     with open(savestring+'0_3.txt', 'a') as file:
-    #         file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-    #     time.sleep(waittime)
 
-    # Random probing at 0.5N
-    targetforce = 0.5  # In N
-    for i in range(595, 1000):
+
+    # Random probing at 0.3N
+    targetforce = 0.3  # In N
+    for i in range(1000):
         print(i)
         x = 18.16*random.random() + 1  # 22.86 side length
         y = 25.78*random.random() + 1  # 30.48 side length
         Ender.write(str.encode("G1 X "+str(Cornerposition[0]+x)+" Y "+str(Cornerposition[1]+y)+" F800\r\n"))
         waitforposition()
         times = takereading(targetforce)
-        with open(savestring+'0_5.txt', 'a') as file:
+        with open(savestring+'0_3.txt', 'a') as file:
             file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
         time.sleep(waittime)
 
-    # Random probing at 0.7N
-    targetforce = 0.7  # In N
-    for i in range(1000):
+    # Random depth probing
+    for i in range(10000):
+        targetforce = 1.0*random.random()  # Random force up to 1.0 N
         print(i)
-        x = 18.16 * random.random() + 1  # 22.86 side length
-        y = 25.78 * random.random() + 1  # 30.48 side length
-        Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+        x = 18.16*random.random() + 1  # 22.86 side length
+        y = 25.78*random.random() + 1  # 30.48 side length
+        Ender.write(str.encode("G1 X "+str(Cornerposition[0]+x)+" Y "+str(Cornerposition[1]+y)+" F800\r\n"))
         waitforposition()
         times = takereading(targetforce)
-        with open(savestring + '0_7.txt', 'a') as file:
-            file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+        with open(savestring+'random.txt', 'a') as file:
+            file.write('%s, %s, %s, %s, %s, %s\n' % (str(x), str(y), str(targetforce), times[0], times[1], times[2]))
         time.sleep(waittime)
 
-    # Random probing at 1N
-    targetforce = 1.0  # In N
-    for i in range(1000):
-        print(i)
-        x = 18.16 * random.random() + 1  # 22.86 side length
-        y = 25.78 * random.random() + 1  # 30.48 side length
-        Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
-        waitforposition()
-        times = takereading(targetforce)
-        with open(savestring + '1_0.txt', 'a') as file:
-            file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-        time.sleep(waittime)
-
-    # Repetition data: 3 points, 4 forces, 5 repetitions
-    xs = [5, 5, 10]
-    ys = [5, 15, 15]
-    for i in range(5):
-        for j in range(3):
-
-            x = xs[j]
-            y = ys[j]
-
-            targetforce = 0.3
-            Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
-            waitforposition()
-            times = takereading(targetforce)
-            with open(savestring + 'repeats.txt', 'a') as file:
-                file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-            time.sleep(waittime)
-
-            targetforce = 0.5
-            Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
-            waitforposition()
-            times = takereading(targetforce)
-            with open(savestring + 'repeats.txt', 'a') as file:
-                file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-            time.sleep(waittime)
-
-            targetforce = 0.7
-            Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
-            waitforposition()
-            times = takereading(targetforce)
-            with open(savestring + 'repeats.txt', 'a') as file:
-                file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-            time.sleep(waittime)
-
-            targetforce = 1.0
-            Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
-            waitforposition()
-            times = takereading(targetforce)
-            with open(savestring + 'repeats.txt', 'a') as file:
-                file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
-            time.sleep(waittime)
+    # # Random probing at 0.5N
+    # targetforce = 0.5  # In N
+    # for i in range(595, 1000):
+    #     print(i)
+    #     x = 18.16*random.random() + 1  # 22.86 side length
+    #     y = 25.78*random.random() + 1  # 30.48 side length
+    #     Ender.write(str.encode("G1 X "+str(Cornerposition[0]+x)+" Y "+str(Cornerposition[1]+y)+" F800\r\n"))
+    #     waitforposition()
+    #     times = takereading(targetforce)
+    #     with open(savestring+'0_5.txt', 'a') as file:
+    #         file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #     time.sleep(waittime)
+    #
+    # # Random probing at 0.7N
+    # targetforce = 0.7  # In N
+    # for i in range(1000):
+    #     print(i)
+    #     x = 18.16 * random.random() + 1  # 22.86 side length
+    #     y = 25.78 * random.random() + 1  # 30.48 side length
+    #     Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #     waitforposition()
+    #     times = takereading(targetforce)
+    #     with open(savestring + '0_7.txt', 'a') as file:
+    #         file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #     time.sleep(waittime)
+    #
+    # # Random probing at 1N
+    # targetforce = 1.0  # In N
+    # for i in range(1000):
+    #     print(i)
+    #     x = 18.16 * random.random() + 1  # 22.86 side length
+    #     y = 25.78 * random.random() + 1  # 30.48 side length
+    #     Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #     waitforposition()
+    #     times = takereading(targetforce)
+    #     with open(savestring + '1_0.txt', 'a') as file:
+    #         file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #     time.sleep(waittime)
+    #
+    # # Repetition data: 3 points, 4 forces, 5 repetitions
+    # xs = [5, 5, 10]
+    # ys = [5, 15, 15]
+    # for i in range(5):
+    #     for j in range(3):
+    #
+    #         x = xs[j]
+    #         y = ys[j]
+    #
+    #         targetforce = 0.3
+    #         Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #         waitforposition()
+    #         times = takereading(targetforce)
+    #         with open(savestring + 'repeats.txt', 'a') as file:
+    #             file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #         time.sleep(waittime)
+    #
+    #         targetforce = 0.5
+    #         Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #         waitforposition()
+    #         times = takereading(targetforce)
+    #         with open(savestring + 'repeats.txt', 'a') as file:
+    #             file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #         time.sleep(waittime)
+    #
+    #         targetforce = 0.7
+    #         Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #         waitforposition()
+    #         times = takereading(targetforce)
+    #         with open(savestring + 'repeats.txt', 'a') as file:
+    #             file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #         time.sleep(waittime)
+    #
+    #         targetforce = 1.0
+    #         Ender.write(str.encode("G1 X " + str(Cornerposition[0] + x) + " Y " + str(Cornerposition[1] + y) + " F800\r\n"))
+    #         waitforposition()
+    #         times = takereading(targetforce)
+    #         with open(savestring + 'repeats.txt', 'a') as file:
+    #             file.write('%s, %s, %s, %s, %s\n' % (str(x), str(y), times[0], times[1], times[2]))
+    #         time.sleep(waittime)
 
 setup()
 main()
